f653c76772052484310907c29e901eb66ebdd34f
AbdullahVC <69648705+AbdullahVC@users.noreply.github.com>
2024-07-16 20:06:50 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Abdullah Veysel Çoşkun Hayalim olan yazılım işine girebilecek bir seviyede olmak için");
+        }
+    }
+}
