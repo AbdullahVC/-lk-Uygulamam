f4a0c4bae03c36778ce69cc74570c8c70a0c4956
AbdullahVC <69648705+AbdullahVC@users.noreply.github.com>
2024-07-16 20:23:42 +0300

    Yorum Eklendi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            // Burada Week1 sekmesindeki İlk Uygulamam Ödevi
+            // Amaç Consola yazı yazdırmak
             Console.WriteLine(" Abdullah Veysel Çoşkun");
             Console.WriteLine(" Hayalim olan yazılım işine girebilecek bir seviyede olmak için bu bootcampe katıldım.");
         }
